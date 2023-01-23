6d2cd634e0265f255a0578a2acb19cb8c26379e0
Moshood <moshoodakeemolayinka@gmail.com>
2023-01-23 22:17:49 +0100

    use mixins generic class

--- todos/urls.py
+++ todos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.TodoDetailAPIView.as_view(), name='get_todo'),
-    path('<int:pk>/update/', views.TodoUpdateAPIView.as_view(), name='get_todo'),
-    path('<int:pk>/delete/', views.TodoDeleteAPIView.as_view(), name='get_todo'),
-    path('create/', views.TodoCreateAPIView.as_view(), name='creat_todo'),
-    path('', views.TodoListAPIView.as_view(), name='todos'),
+    path('<int:pk>/', views.TodoMixinsViews.as_view(), name='get_todo'),
+    path('<int:pk>/update/', views.TodoMixinsViews.as_view(), name='get_todo'),
+    path('<int:pk>/delete/', views.TodoMixinsViews.as_view(), name='get_todo'),
+    path('create/', views.TodoMixinsViews.as_view(), name='creat_todo'),
+    path('', views.TodoMixinsViews.as_view(), name='todos'),
     path('create_or_get/', views.TodoListCreateAPIView().as_view(), name='both')
 ]
