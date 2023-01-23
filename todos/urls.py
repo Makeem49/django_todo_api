08599ff1c195affeb9c7ea1e6df4d8d793631166
Moshood <moshoodakeemolayinka@gmail.com>
2023-01-23 15:46:29 +0100

    add url for creating todo

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_todo, name='create_todo')
+    path('', views.get_todo, name='get_todo'),
+    path('create/', views.create_todo, name='create_todo'),
 ]
