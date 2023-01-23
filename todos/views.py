71ed81a44ecf350f55e326d149ad81dd2c68bc0a
Moshood <moshoodakeemolayinka@gmail.com>
2023-01-23 13:11:19 +0100

    add rest decorator and response

--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Todo
 # Create your views here.
 
-
+@api_view(['GET'])
 def create_todo(request):
     """The process of converting a model instance 
     to a dictionary and later to json format is called serialization. 
@@ -18,4 +20,4 @@ def create_todo(request):
         data = model_to_dict(model_instance, fields=['name', 'created_at'])
 
         
-    return JsonResponse(data) # converting to json format and send o client 
+    return Response(data) # converting to json format and send o client 
