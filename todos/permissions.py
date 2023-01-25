fcb5ed60ebf4d2c70b00b512219950db53164e36
Moshood <moshoodakeemolayinka@gmail.com>
2023-01-25 11:44:27 +0100

    comment out has_permission so that perms_map will take over

--- todos/permissions.py
+++ todos/permissions.py
@@ -13,33 +13,33 @@ class IsStaffEdit(permissions.DjangoModelPermissions):
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
 
-    def has_permission(self, request, view):
-        """This will check if the user staff user, else return false 
+    # def has_permission(self, request, view):
+    #     """This will check if the user staff user, else return false 
         
-        which will make the user unable to have a permission to perform user 
+    #     which will make the user unable to have a permission to perform user 
         
-        activities.
+    #     activities.
 
-        """
-        if not request.user.is_staff:
-            return False
-        return super().has_permission(request, view)
+    #     """
+    #     if not request.user.is_staff:
+    #         return False
+    #     return super().has_permission(request, view)
 
-    def has_permission(self, request, view):
-        user = request.user 
-        if not user.is_staff:
-            return False
+    # def has_permission(self, request, view):
+    #     user = request.user 
+    #     if not user.is_staff:
+    #         return False
 
-        if user.is_staff:
-            """If user is a staff, and also have permissions to do 
-            a lot of thing such has add, view, delete, update. 
-            """
-            if user.has_perm('todos.add_todo'):
-                return True
-            if user.has_perm('todos.view_todo'):
-                return True
-            if user.has_perm('todos.change_todo'):
-                return True
-            if user.has_perm('todos.delete_todo'):
-                return True
-        return False 
+    #     if user.is_staff:
+    #         """If user is a staff, and also have permissions to do 
+    #         a lot of thing such has add, view, delete, update. 
+    #         """
+    #         if user.has_perm('todos.add_todo'):
+    #             return True
+    #         if user.has_perm('todos.view_todo'):
+    #             return True
+    #         if user.has_perm('todos.change_todo'):
+    #             return True
+    #         if user.has_perm('todos.delete_todo'):
+    #             return True
+    #     return False 
