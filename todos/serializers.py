b21377553d2cc370db6873eceb5005cf0092aaab
Moshood <moshoodakeemolayinka@gmail.com>
2023-01-25 16:26:37 +0100

    adding url field and updata name of enpoints

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Todo
 
@@ -7,15 +8,26 @@ class TodoSerializer(serializers.ModelSerializer):
     format it properly
     """
     completed = serializers.SerializerMethodField(read_only=True) 
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name="todo-detail", lookup_field='pk')
+
     class Meta:
         model = Todo
         fields = [
+            "url",
+            "edit_url",
             "name",
             "created_at",
             "target_time",
             "completed",
             "is_done"
         ]
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request') # self.request
+        if request is None:
+            return None
+        return reverse('todo-edit', kwargs={"pk" : obj.pk}, request=request)
 
     def get_completed(self, obj):
         if hasattr(obj, "id"):
--- todos/urls.py
+++ todos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.TodoDetailAPIView.as_view(), name='get_todo'),
-    path('<int:pk>/update/', views.TodoUpdateAPIView.as_view(), name='get_todo'),
-    path('<int:pk>/delete/', views.TodoDeleteAPIView.as_view(), name='get_todo'),
-    path('create/', views.TodoCreateAPIView.as_view(), name='creat_todo'),
-    path('', views.TodoListAPIView.as_view(), name='todos'),
+    path('<int:pk>/', views.TodoDetailAPIView.as_view(), name='todo-detail'),
+    path('<int:pk>/update/', views.TodoUpdateAPIView.as_view(), name='todo-edit'),
+    path('<int:pk>/delete/', views.TodoDeleteAPIView.as_view(), name='todo-delete'),
+    path('create/', views.TodoCreateAPIView.as_view(), name='todo-create'),
+    path('', views.TodoListAPIView.as_view(), name='todo-list'),
     path('create_or_get/', views.TodoListCreateAPIView().as_view(), name='both')
 ]
